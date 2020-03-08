2932e3dc1f391fed7248decc1d1b0e57d608c5e9
Admin <Admin@192.168.0.10>
2020-03-08 12:59:21 +0530

    Commit 4

--- google/src/test/java/com/google/testcases/GmailAccountCreation.java
+++ google/src/test/java/com/google/testcases/GmailAccountCreation.java
@@ -6,6 +6,7 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -22,7 +23,7 @@ public class GmailAccountCreation {
 	public static ExtentReports report;
 
 	// Initializing the setup
-	@BeforeMethod
+	@BeforeTest
 	@Parameters({ "Browser", "Url" })
 	public static void setup(String browser, String url) {
 		
