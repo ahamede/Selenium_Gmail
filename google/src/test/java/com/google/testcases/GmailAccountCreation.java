b7f4566afb92b0b2eed93e146b5b120aea6b2a14
Admin <Admin@192.168.0.10>
2020-03-08 11:44:37 +0530

    Commit 3

--- google/src/test/java/com/google/testcases/GmailAccountCreation.java
+++ google/src/test/java/com/google/testcases/GmailAccountCreation.java
@@ -4,24 +4,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.google.lib.BrowserFactory;
 import com.google.pages.gmailpage;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class GmailAccountCreation {
 
 	 static WebDriver driver;
-
+     public static ExtentTest logger;
+     public static ExtentReports report;
+      
 
 
 	@BeforeMethod
 	@Parameters({ "Browser" , "Url"})
 	public static void setup(String browser, String url) {
         driver=BrowserFactory.startBrowser(browser, url);
-		
+        report = new ExtentReports(System.getProperty("user.dir")+"\\ExtentReportResults.html");
+        logger = report.startTest("GmailAccountCreation");
 	}
 
 	@Test
@@ -31,15 +38,14 @@ public class GmailAccountCreation {
 		gmailpage g = PageFactory.initElements(driver, gmailpage.class);
 		g.basicdetails(firstname, lastname, username1, password1);
 		g.enter_phone_number(mobilenumber);
-
+        logger.log(LogStatus.INFO, "Account Created Successfully");
 	}
 
 
 	@AfterMethod
-	public void tearDownReport(ITestResult result) {
-
-	
+	public void tearDownReport() {
 		driver.quit();
+		report.endTest(logger);
 
 	}
 
