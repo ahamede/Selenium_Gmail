6aa1a4e96b6a719daa2bbffdadf02336b7e59315
Admin <Admin@192.168.0.10>
2020-03-10 09:38:39 +0530

    Commit 6

--- google/src/test/java/com/google/lib/Utility.java
+++ google/src/test/java/com/google/lib/Utility.java
@@ -1,5 +1,12 @@
 package com.google.lib;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.ErrorCodes;
 import org.testng.asserts.SoftAssert;
 
@@ -33,28 +40,25 @@ public class Utility {
 
 	// **********************************************************************************
 
-	
-
-	// *********************************************************************************
-
-
-
-	
-	
-
-	// *******************************************************************************************************************************************************
-
-	
-
-	// *********************************************************************************************************************************
-
-	
-
-	// ********************************************************************************************************************************
-
-	
-
-	// ********************************************************************************************************************************
-
+	public static String getScreenshot(WebDriver driver)
+	{
+		TakesScreenshot ts=(TakesScreenshot) driver;
+		
+		File src=ts.getScreenshotAs(OutputType.FILE);
+		
+		String path=System.getProperty("user.dir")+"/Screenshot/"+System.currentTimeMillis()+".png";
+		
+		File destination=new File(path);
+		
+		try 
+		{
+			FileUtils.copyFile(src, destination);
+		} catch (IOException e) 
+		{
+			System.out.println("Capture Failed "+e.getMessage());
+		}
+		
+		return path;
+	}
 
 }
--- google/src/test/java/com/google/testcases/GmailAccountCreation.java
+++ google/src/test/java/com/google/testcases/GmailAccountCreation.java
@@ -3,14 +3,12 @@ package com.google.testcases;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import com.google.lib.BrowserFactory;
+import com.google.lib.Utility;
 import com.google.pages.gmailpage;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
@@ -44,14 +42,18 @@ public class GmailAccountCreation {
 
 		// Enter phone number
 		g.enter_phone_number(mobilenumber);
-		logger.log(LogStatus.INFO, "Account Created Successfully");
+	
+		logger.log(LogStatus.FAIL, Utility.getScreenshot(driver)+"Test Failed");
 	}
 
 	@AfterTest
-	public void tearDownReport() {
-		driver.quit();
-		report.endTest(logger);
-        report.flush();
+	public void tearDownReport() 
+	{
+driver.quit();
+report.flush();
 	}
+	
+	}
+	
 
-}
+
